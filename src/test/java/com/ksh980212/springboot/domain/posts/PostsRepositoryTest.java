9886ba5adfa938fe4d7a72285ce566d9c19a83d0
ksh980212 <ksh980212@gmail.com>
2019-12-25 21:35:00 +0900

    Posts 등록 화면 구성(mustache)

--- src/test/java/com/ksh980212/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/ksh980212/springboot/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.ksh980212.springboot.domain.posts;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.Test;
@@ -41,5 +42,26 @@ public class PostsRepositoryTest {
     Posts posts = postsList.get(0);
     assertThat(posts.getTitle()).isEqualTo(title);
     assertThat(posts.getContent()).isEqualTo(content);
+  }
+
+  @Test
+  public void BaseTimeEntity_등록(){
+    //given
+    LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+    postsRepository.save(Posts.builder()
+            .title("title")
+            .content("content")
+            .author("author")
+            .build());
+
+    //when
+    List<Posts> postsList = postsRepository.findAll();
+
+    //then
+    Posts posts = postsList.get(0);
+
+    System.out.println(">>>>>>>>>> createDate=" + posts.getCreatedDate() + ", " + posts.getModifiedDate());
+    assertThat(posts.getCreatedDate()).isAfter(now);
+    assertThat(posts.getModifiedDate()).isAfter(now);
   }
 }
