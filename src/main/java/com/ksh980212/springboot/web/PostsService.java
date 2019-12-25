59f74b4615379ef92796625f7f690f9386651a50
ksh980212 <ksh980212@gmail.com>
2019-12-25 20:00:22 +0900

    Posts 수정 / 조회 기능 추가

--- src/main/java/com/ksh980212/springboot/web/PostsService.java
+++ src/main/java/com/ksh980212/springboot/web/PostsService.java
@@ -1,7 +1,10 @@
 package com.ksh980212.springboot.web;
 
+import com.ksh980212.springboot.domain.posts.Posts;
 import com.ksh980212.springboot.domain.posts.PostsRepository;
+import com.ksh980212.springboot.web.dto.PostsResponseDto;
 import com.ksh980212.springboot.web.dto.PostsSaveRequestDto;
+import com.ksh980212.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,21 @@ public class PostsService {
   @Transactional
   public Long save(PostsSaveRequestDto requestDto){
     return postsRepository.save(requestDto.toEntity()).getId();
+  }
+
+  @Transactional
+  public Long update(Long id, PostsUpdateRequestDto requestDto){
+    Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+    posts.update(requestDto.getTitle(), requestDto.getContent());
+
+    return id;
+  }
+
+  public PostsResponseDto findById(Long id){
+    Posts entity = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+    return new PostsResponseDto(entity);
   }
 }
