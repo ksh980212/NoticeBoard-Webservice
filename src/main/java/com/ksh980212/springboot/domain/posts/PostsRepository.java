3770e5e0a1fd06bd3d9147074af15ce990c61509
ksh980212 <ksh980212@gmail.com>
2019-12-25 19:32:28 +0900

    첫 커밋 ~ Posts 등록 API 구현
    p1 ~ p110

---
+++ src/main/java/com/ksh980212/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.ksh980212.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
